c93dafe8c4041eb24902d5ff08dd6fef254c7da2
yaelKlain <yael040904@gamil.com>
2024-03-05 10:31:37 +0200

    mytext

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const userRoutes = require()
+const app= express()
+app.use(express.json())
+app.use('/users',userRoutes)
+app.listen(8000,()=>{
+    console.log("port 8000")
+})
+
